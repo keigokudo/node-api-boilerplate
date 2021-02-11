d12fec5fcbdb195dceb98c4e03552872be87ab78
keigokudo <keigokudo55@googlemail.com>
2021-02-11 17:25:23 +0100

    add morgan logger

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const logger = require('morgan')
 
 const sampleRoutes = require('./routes/sample')
 
+app.use(logger('dev'))
 app.use('/sample', sampleRoutes)
 
 module.exports = app
