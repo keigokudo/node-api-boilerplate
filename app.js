4ad7fcb6e9856c8e109af4f1c1af5a084aba1a61
keigokudo <keigokudo55@googlemail.com>
2021-02-11 17:38:34 +0100

    handle error

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ const sampleRoutes = require('./routes/sample')
 app.use(logger('dev'))
 app.use('/sample', sampleRoutes)
 
+app.use((req, res, next) => {
+  const error = new Error('Route not found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message,
+    },
+  })
+})
+
 module.exports = app
