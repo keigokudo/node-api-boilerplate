0a20e93a096c57edd8215db21025229f4430b999
keigokudo <keigokudo55@googlemail.com>
2021-02-12 10:39:51 +0100

    use body-parser

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const logger = require('morgan')
+const bodyParser = require('body-parser')
 
 const sampleRoutes = require('./routes/sample')
 
+// middlewares
 app.use(logger('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// routes
 app.use('/sample', sampleRoutes)
 
+// error handling
 app.use((req, res, next) => {
   const error = new Error('Route not found')
   error.status = 404
