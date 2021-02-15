dffc53ee0682c0efe1f7c8921492a523fd1acbde
keigokudo <keigokudo55@googlemail.com>
2021-02-15 10:22:05 +0100

    set basic validation

--- models/sample.js
+++ models/sample.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const sampleSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  number: Number,
+  name: { type: String, required: true },
+  number: { type: Number, required: true },
 })
 
 module.exports = mongoose.model('Sample', sampleSchema)
