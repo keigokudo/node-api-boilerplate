3729b4d5979186bd01824e3810967cd6c136454c
keigokudo <keigokudo55@googlemail.com>
2021-02-19 17:41:19 +0100

    refactor bcrypt with hashSync

--- routes/user.js
+++ routes/user.js
@@ -6,30 +6,38 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({ error: err })
-    } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash,
-      })
-      user
-        .save()
-        .then((result) => {
-          res.status(200).json({
-            message: 'user created',
-          })
-        })
-        .catch((err) => {
-          console.log(err)
-          res.status(500).json({
-            message: err,
-          })
-        })
-    }
+  //   User.find({ email: req.body.email })
+  //     .exec()
+  //     .then((user) => {
+  //       if (user) {
+  //         return res.status(409).json({
+  //           message: 'E-mail already exsists',
+  //         })
+  //       }
+  //     })
+
+  const saltRounds = 10
+  const hash = bcrypt.hashSync(req.body.password, saltRounds)
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    email: req.body.email,
+    password: hash,
   })
+
+  user
+    .save()
+    .then((result) => {
+      res.status(200).json({
+        message: 'user created',
+        result: result,
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({
+        message: err,
+      })
+    })
 })
 
 module.exports = router
