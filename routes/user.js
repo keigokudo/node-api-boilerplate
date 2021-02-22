ab1a7328b9856483bbe9b69eb1a6995cf67d2d48
keigokudo <keigokudo55@googlemail.com>
2021-02-22 13:42:36 +0100

    refactor email validation

--- routes/user.js
+++ routes/user.js
@@ -6,55 +6,6 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 router.post('/signup', async (req, res, next) => {
-  //   User.find({ email: req.body.email })
-  //     .exec()
-  //     .then((user) => {
-  //       if (user) {
-  //         return res.status(409).json({
-  //           message: 'E-mail already exsists',
-  //         })
-  //       }
-  //     })
-  // try {
-  //   const post = await User.findOne({ email: req.body.email })
-  //   if (post.email) {
-  //     throw new Error('e-mail')
-  //   }
-  // } catch (err) {
-  //   console.log('err::', err)
-  // }
-
-  // try {
-  //   const post = await User.findOne({ email: req.body.email })
-  //   if (post.email) {
-  //     res.status(409).json({
-  //       message: 'E-mail already exsists',
-  //     })
-  //     throw new Error('email')
-  //   }
-  // } catch (err) {
-  //   reject(err)
-  // }
-
-  // User.findOne({ email: req.body.email })
-  //   .then((post) => {
-  //     console.log('post', post)
-  //     if (post.email) {
-  //       return res.status(409).json({
-  //         message: 'E-mail already exsists',
-  //       })
-  //     }
-  //     return next()
-  //   })
-  //   .catch(next)
-
-  // if (post.email) {
-  //   throw res.status(409).json({
-  //     message: 'E-mail already exsists',
-  //   })
-  // }
-  // console.log('post.email:::', post.email)
-
   const saltRounds = 10
   const hash = bcrypt.hashSync(req.body.password, saltRounds)
   const user = new User({
